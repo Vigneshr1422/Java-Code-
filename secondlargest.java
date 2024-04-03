f705428ccc2bd9d6488b97c60ab69bf98ae16b7e
R Vignesh <135974066+Vigneshr1422@users.noreply.github.com>
2024-04-03 06:36:46 +0530

    Find the Second largest array in user input

---
+++ secondlargest.java
@@ -0,0 +1,25 @@
+package arrays;
+import java.util.*;
+public class secondlargest {
+   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("The size of array : ");
+    int size = in.nextInt();
+    int arr[]= new int[size];
+    System.out.println("The array elements : ");
+    for(int i=0;i<size;i++)
+    {
+        arr[i]=in.nextInt();
+    }
+    Arrays.sort(arr);
+    System.out.println("The sorted array : ");
+    for(int i=0;i<size;i++)
+    {
+        System.out.println(arr[i]+" ");
+    }
+    System.out.println("The second largest array is : "+arr[size-2]);
+    System.out.println("The second smallest  array is : "+arr[1]);
+  
+
+   } 
+}
