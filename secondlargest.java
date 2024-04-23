ae6dd661377a1e1a015a6adbf8e235fe2c0e6066
R Vignesh <135974066+Vigneshr1422@users.noreply.github.com>
2024-04-23 21:11:38 +0530

    find the second largest element in an array.

--- secondlargest.java
+++ secondlargest.java
@@ -1,23 +1,32 @@
-package arrays;
+package summ;
 import java.util.*;
 public class secondlargest {
-   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.println("The size of array : ");
-    int size = in.nextInt();
-    int arr[]= new int[size];
-    System.out.println("The array elements : ");
-    for(int i=0;i<size;i++)
-    {
-        arr[i]=in.nextInt();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter the size : ");
+        int size = in.nextInt();
+        int [] arr = new int[size];
+        System.out.print("Enter the array elements : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=in.nextInt();
+        }
+        System.out.print("The array elements are : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+        Arrays.sort(arr);
+        System.out.println(" ");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(+arr[i]+" ");
+
+        }
+        System.out.println("the second largest elements is "+arr[size-2]);
+
+        
+        in.close();
     }
-    Arrays.sort(arr);
-    System.out.println("The sorted array : ");
-    for(int i=0;i<size;i++)
-    {
-        System.out.println(arr[i]+" ");
-    }
-    System.out.println("The second largest array is : "+arr[size-2]);
-    in.close();
-   } 
 }
