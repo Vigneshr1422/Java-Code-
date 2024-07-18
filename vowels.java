962fe2dbc5dc5d1fce568e8bfa2dcb9724948f35
R Vignesh <135974066+Vigneshr1422@users.noreply.github.com>
2024-07-18 21:44:05 +0530

    vowels.java
    Count Vowels using Various Approach

--- vowels.java
+++ vowels.java
@@ -3,26 +3,48 @@ public class vowels {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the String : ");
-        String a1 = in.nextLine();
-        char op[]=a1.toCharArray();
-        int count=0 ;
-        for (char c : op) {
-            switch (c) {
-                   case 'a':
-                   case 'e':
-                   case 'i':
-                   case 'o':
-                   case 'u':
-                   count++;            
-                default:
-                    break;
-            }
-        }
-        System.out.println("The vowels are given string is "+count);
-       // System.out.println("The vowels are given string count is "+count);
-        
-        
-        in.close();
+        String a= in.nextLine();
+        System.out.println("The Vowels Count is "+method1(a));
+        System.out.println("The Vowels Count using loop is " + method2(a));
     }
-    
+    public static int method1(String a)
+    {
+        int count=0;
+        char []b=a.toCharArray();
+        for(char x : b)
+            {
+                switch(x)
+                    {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'i':
+                    case 'u':
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                    count++;
+                    break;
+                default :
+                    break;
+                    }
+            }
+        return count;
+    }
+     public static int method2(String a)
+    {
+            int b=a.length();
+            int count=0;
+            for(int i=0;i<b;i++)
+                {
+                    char x=a.charAt(i);
+                    if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u' || x=='A' || x=='E' || x=='I' || x=='O' || x=='U' )
+                    {
+                        count++;
+                    }
+                }
+            return count;
+    }
 }
